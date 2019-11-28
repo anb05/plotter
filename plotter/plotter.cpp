63726c8977bb27698f973af1c89b89babd062a83
bovkun <alexandr05@list.ru>
2019-11-28 17:42:28 +0200

    fixing commit

--- plotter/plotter.cpp
+++ plotter/plotter.cpp
@@ -2,6 +2,8 @@
 
 #include <QIcon>
 #include <QToolButton>
+#include <QStylePainter>
+#include <QStyleOptionFocusRect>
 
 namespace plt {
 
@@ -31,42 +33,74 @@ Plotter::Plotter(QWidget *parent)
 
 void Plotter::setPlotSettings(const PlotSettings& settings)
 {
-
+	_zoomStack.clear();
+	_zoomStack.append(settings);
+	_curZoom = 0;
+	_pZoomInBtn->hide();
+	_pZoomOutBtn->hide();
+	refreshPixmap();
 }
 
 void Plotter::setCurveData(int id, const QVector<QPointF>& data)
 {
-
+	_curveMap[id] = data;
+	refreshPixmap();
 }
 
 void Plotter::clearCurve(int id)
 {
-
+	_curveMap.remove(id);
+	refreshPixmap();
 }
 
 QSize Plotter::minimumSizeHint() const
 {
-
+	return QSize(6 * MARGIN, 4 * MARGIN);
 }
 
 QSize Plotter::sizeHint() const
 {
-
+	return QSize(12 * MARGIN, 8 * MARGIN);
 }
 
 void Plotter::zoomIn()
 {
-
+	if (_curZoom < _zoomStack.count() - 1) {
+		++_curZoom;
+		_pZoomInBtn->setEnabled(_zoomStack.count() - 1);
+		_pZoomOutBtn->setEnabled(true);
+		_pZoomOutBtn->show();
+		refreshPixmap();
+	}
 }
 
 void Plotter::zoomOut()
 {
-
+	if (_curZoom > 0 ) {
+		--_curZoom;
+		_pZoomOutBtn->setEnabled(_curZoom > 0);
+		_pZoomInBtn->setEnabled(true);
+		_pZoomInBtn->show();
+		refreshPixmap();
+	}
 }
 
 void Plotter::paintEvent(QPaintEvent* event)
 {
+	QStylePainter painter(this);
+	painter.drawPixmap(0, 0, _pixmap);
 
+	if (_rubberBandIsShown) {
+		painter.setPen(palette().light().color());
+		painter.drawRect(_rubberBandRect.normalized().adjusted(0, 0, -1, -1));
+	}
+
+	if (hasFocus()) {
+		QStyleOptionFocusRect option;
+		option.initFrom(this);
+		option.backgroundColor = palette().dark().color();
+		painter.drawPrimitive(QStyle::PE_FrameFocusRect, option);
+	}
 }
 
 void Plotter::resizeEvent(QResizeEvent* event)
